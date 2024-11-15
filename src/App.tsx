e93b68b33ac4aa8447527b44158cdf04423a1e38
Arseniy <arseniy.medyakov@gmail.com>
2024-11-16 01:23:17 +0300

    implemented the functionality and adaptability of the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./styles/App.scss";
+import Header from "./components/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <h1 className="text-red-500">Проверка Tailwind</h1>
-      <div className="bg-green-500 h-10 w-10"></div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
     </>
   );
 }
@@ -36,6 +15,8 @@ function App() {
 //       <Routes>
 //         <Route path="/" element={<Home />} />
 //         <Route path="/portfolio" element={<Portfolio />} />
+//          <Route path="/portfolio/visual" element={<PortfolioVisual />} />
+//          <Route path="/portfolio/mobile" element={<PortfolioPhoto />} />
 //         <Route path="/services" element={<Services />} />
 //         <Route path="/about" element={<About />} />
 //       </Routes>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,3 +4,97 @@
 // Ссылки на социальные сети.
 // Навигация по разделам (например, "Портфолио", "Услуги и цены", "Контакты", "Обо мне").
 // Мобильная версия, где меню сворачивается в выпадающий список.
+
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "../styles/Header.scss";
+
+const Header: React.FC = () => {
+  useEffect(() => {
+    const headerElement = document.querySelector(".header");
+    if (headerElement) {
+      headerElement.classList.add("header--visible");
+    }
+  }, []);
+
+  const [isPortfolioOpen, setIsPortfolioOpen] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  useEffect(() => {
+    if (isMobileMenuOpen) {
+      setIsPortfolioOpen(true);
+    }
+  }, [isMobileMenuOpen]);
+
+  const togglePortfolioMenu = () => {
+    setIsPortfolioOpen((prev) => !prev);
+  };
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen((prev) => !prev);
+  };
+
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <Link to="/">dashkens</Link>
+      </div>
+      <button
+        className={`header__burger ${isMobileMenuOpen ? "active" : ""}`}
+        onClick={toggleMobileMenu}
+      >
+        <span className="header__burger-line"></span>
+        <span className="header__burger-line"></span>
+        <span className="header__burger-line"></span>
+      </button>
+      <nav
+        className={`header__nav ${isMobileMenuOpen ? "header__nav--open" : ""}`}
+      >
+        <ul className="header__nav-list">
+          <li>
+            <div className="header__dropdown">
+              <span
+                className="header__link"
+                onClick={() => togglePortfolioMenu}
+                onMouseEnter={() => setIsPortfolioOpen(true)}
+                onMouseLeave={() => setIsPortfolioOpen(false)}
+              >
+                Портфолио
+                <span
+                  className={`header__arrow ${
+                    isPortfolioOpen ? "header__arrow--open" : ""
+                  }`}
+                >
+                  ↓
+                </span>
+              </span>
+              <ul
+                className={`header__dropdown-menu ${
+                  isPortfolioOpen ? "header__dropdown-menu--visible" : ""
+                }`}
+                onMouseEnter={() => setIsPortfolioOpen(true)}
+                onMouseLeave={() => setIsPortfolioOpen(false)}
+              >
+                <li className="header__dropdown-menu-li">
+                  <Link to="/portfolio/visual">Визуальные концепции</Link>
+                </li>
+                <li className="header__dropdown-menu-li">
+                  <Link to="/portfolio/mobile">Мобильные съемки</Link>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <Link to="/services">Услуги</Link>
+          </li>
+          <li>
+            <Link to="/about">Контакты</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
