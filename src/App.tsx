3659d54113447ba7a2588697e181f9f637b04774
Arseniy <arseniy.medyakov@gmail.com>
2025-07-26 09:18:52 +0300

    fix building bug

--- src/App.tsx
+++ src/App.tsx
@@ -13,33 +13,42 @@ const PortfolioReels = lazy(() => import('./pages/PortfolioReels'));
 const PortfolioVisual = lazy(() => import('./pages/PortfolioVisual'));
 const Gallery = lazy(() => import('./components/Gallery'));
 
-const GalleryLoader: React.FC<{ dataPath: string }> = ({ dataPath }) => {
+const mediaModules = import.meta.glob('./(Photos|Reels|Visual)/*.ts');
+
+interface GalleryLoaderProps {
+  modulePath: string;
+}
+
+const GalleryLoader: React.FC<GalleryLoaderProps> = ({ modulePath }) => {
   const [media, setMedia] = useState<{ images?: any[]; videos?: any[] } | null>(null);
+
+  const loadModule = mediaModules[modulePath];
 
   useEffect(() => {
     let isMounted = true;
-    const loadMedia = async () => {
-      try {
-        const module = await import(`${dataPath}`);
 
-        if (isMounted) {
-          setMedia({
-            images: module.default.photos,
-            videos: module.default.videos,
-          });
-        }
-      } catch (error) {
-        console.error('Failed to load gallery data:', error);
-        if (isMounted) setMedia({ images: [], videos: [] });
-      }
-    };
-
-    loadMedia();
+    if (loadModule) {
+      loadModule()
+        .then((module: any) => {
+          if (isMounted) {
+            setMedia({
+              images: module.default.photos,
+              videos: module.default.videos,
+            });
+          }
+        })
+        .catch((error) => {
+          console.error('Failed to load gallery module:', error);
+          if (isMounted) setMedia({ images: [], videos: [] });
+        });
+    } else {
+      console.error(`Module not found for path: ${modulePath}`);
+    }
 
     return () => {
       isMounted = false;
     };
-  }, [dataPath]);
+  }, [loadModule, modulePath]);
 
   if (!media) {
     return <Spinner />;
@@ -63,43 +72,43 @@ const App: React.FC = () => {
           <Route path="/about" element={<About />} />
 
           {/* Галереи с фото и/или видео */}
-          <Route path="/anastasia" element={<GalleryLoader dataPath="./Photos/anastasia.ts" />} />
-          <Route path="/anastasia2" element={<GalleryLoader dataPath="./Photos/anastasia2.ts" />} />
-          <Route path="/elizabeth" element={<GalleryLoader dataPath="./Photos/elizabeth.ts" />} />
-          <Route path="/dmitry" element={<GalleryLoader dataPath="./Photos/dima.ts" />} />
-          <Route path="/emine" element={<GalleryLoader dataPath="./Photos/emine.ts" />} />
-          <Route path="/jewelryBrand" element={<GalleryLoader dataPath="./Photos/jewelry-brand.ts" />} />
-          <Route path="/tatiana" element={<GalleryLoader dataPath="./Photos/tatiana.ts" />} />
-          <Route path="/cosmetology" element={<GalleryLoader dataPath="./Photos/cosmetology.ts" />} />
-          <Route path="/ekaterina" element={<GalleryLoader dataPath="./Photos/сatherine.ts" />} />
-          <Route path="/lina" element={<GalleryLoader dataPath="./Photos/lina.ts" />} />
+          <Route path="/anastasia" element={<GalleryLoader modulePath="./Photos/anastasia.ts" />} />
+          <Route path="/anastasia2" element={<GalleryLoader modulePath="./Photos/anastasia2.ts" />} />
+          <Route path="/elizabeth" element={<GalleryLoader modulePath="./Photos/elizabeth.ts" />} />
+          <Route path="/dmitry" element={<GalleryLoader modulePath="./Photos/dima.ts" />} />
+          <Route path="/emine" element={<GalleryLoader modulePath="./Photos/emine.ts" />} />
+          <Route path="/jewelryBrand" element={<GalleryLoader modulePath="./Photos/jewelry-brand.ts" />} />
+          <Route path="/tatiana" element={<GalleryLoader modulePath="./Photos/tatiana.ts" />} />
+          <Route path="/cosmetology" element={<GalleryLoader modulePath="./Photos/cosmetology.ts" />} />
+          <Route path="/ekaterina" element={<GalleryLoader modulePath="./Photos/сatherine.ts" />} />
+          <Route path="/lina" element={<GalleryLoader modulePath="./Photos/lina.ts" />} />
 
           {/* Галереи с Reels */}
-          <Route path="/my-self" element={<GalleryLoader dataPath="./Reels/my-self.ts" />} />
-          <Route path="/filtih" element={<GalleryLoader dataPath="./Reels/filtih.ts" />} />
-          <Route path="/sansusi" element={<GalleryLoader dataPath="./Reels/sansusi.ts" />} />
-          <Route path="/kedi-myasoeda" element={<GalleryLoader dataPath="./Reels/kedi-myasoeda.ts" />} />
-          <Route path="/ambroperfume" element={<GalleryLoader dataPath="./Reels/ambroperfume.ts" />} />
+          <Route path="/my-self" element={<GalleryLoader modulePath="./Reels/my-self.ts" />} />
+          <Route path="/filtih" element={<GalleryLoader modulePath="./Reels/filtih.ts" />} />
+          <Route path="/sansusi" element={<GalleryLoader modulePath="./Reels/sansusi.ts" />} />
+          <Route path="/kedi-myasoeda" element={<GalleryLoader modulePath="./Reels/kedi-myasoeda.ts" />} />
+          <Route path="/ambroperfume" element={<GalleryLoader modulePath="./Reels/ambroperfume.ts" />} />
 
           {/* Галереи с кейсами (Visual) */}
-          <Route path="/case1" element={<GalleryLoader dataPath="./Visual/case1.ts" />} />
-          <Route path="/case2" element={<GalleryLoader dataPath="./Visual/case2.ts" />} />
-          <Route path="/case3" element={<GalleryLoader dataPath="./Visual/case3.ts" />} />
-          <Route path="/case4" element={<GalleryLoader dataPath="./Visual/case4.ts" />} />
-          <Route path="/case5" element={<GalleryLoader dataPath="./Visual/case5.ts" />} />
-          <Route path="/case6" element={<GalleryLoader dataPath="./Visual/case6.ts" />} />
-          <Route path="/case7" element={<GalleryLoader dataPath="./Visual/case7.ts" />} />
-          <Route path="/case8" element={<GalleryLoader dataPath="./Visual/case8.ts" />} />
-          <Route path="/case9" element={<GalleryLoader dataPath="./Visual/case9.ts" />} />
-          <Route path="/case10" element={<GalleryLoader dataPath="./Visual/case10.ts" />} />
-          <Route path="/case11" element={<GalleryLoader dataPath="./Visual/case11.ts" />} />
-          <Route path="/case12" element={<GalleryLoader dataPath="./Visual/case12.ts" />} />
-          <Route path="/case13" element={<GalleryLoader dataPath="./Visual/case13.ts" />} />
-          <Route path="/case14" element={<GalleryLoader dataPath="./Visual/case14.ts" />} />
-          <Route path="/case15" element={<GalleryLoader dataPath="./Visual/case15.ts" />} />
-          <Route path="/case16" element={<GalleryLoader dataPath="./Visual/case16.ts" />} />
-          <Route path="/case17" element={<GalleryLoader dataPath="./Visual/case17.ts" />} />
-          <Route path="/case18" element={<GalleryLoader dataPath="./Visual/case18.ts" />} />
+          <Route path="/case1" element={<GalleryLoader modulePath="./Visual/case1.ts" />} />
+          <Route path="/case2" element={<GalleryLoader modulePath="./Visual/case2.ts" />} />
+          <Route path="/case3" element={<GalleryLoader modulePath="./Visual/case3.ts" />} />
+          <Route path="/case4" element={<GalleryLoader modulePath="./Visual/case4.ts" />} />
+          <Route path="/case5" element={<GalleryLoader modulePath="./Visual/case5.ts" />} />
+          <Route path="/case6" element={<GalleryLoader modulePath="./Visual/case6.ts" />} />
+          <Route path="/case7" element={<GalleryLoader modulePath="./Visual/case7.ts" />} />
+          <Route path="/case8" element={<GalleryLoader modulePath="./Visual/case8.ts" />} />
+          <Route path="/case9" element={<GalleryLoader modulePath="./Visual/case9.ts" />} />
+          <Route path="/case10" element={<GalleryLoader modulePath="./Visual/case10.ts" />} />
+          <Route path="/case11" element={<GalleryLoader modulePath="./Visual/case11.ts" />} />
+          <Route path="/case12" element={<GalleryLoader modulePath="./Visual/case12.ts" />} />
+          <Route path="/case13" element={<GalleryLoader modulePath="./Visual/case13.ts" />} />
+          <Route path="/case14" element={<GalleryLoader modulePath="./Visual/case14.ts" />} />
+          <Route path="/case15" element={<GalleryLoader modulePath="./Visual/case15.ts" />} />
+          <Route path="/case16" element={<GalleryLoader modulePath="./Visual/case16.ts" />} />
+          <Route path="/case17" element={<GalleryLoader modulePath="./Visual/case17.ts" />} />
+          <Route path="/case18" element={<GalleryLoader modulePath="./Visual/case18.ts" />} />
         </Routes>
       </Suspense>
     </Router>
